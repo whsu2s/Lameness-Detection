a456209e32ae02ed91a48f49c29fcacd69fee654
whsu2s <weichiliedik@gmail.com>
2019-05-17 15:04:09 +0200

    Revise print_analysis function

--- utils/auxiliaryfunctions.py
+++ utils/auxiliaryfunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix, f1_score
 import shutil
+import os
 
 def tag_loc(tag):
     ''' Return the corresponding location in csv file for the n-th day data'''
@@ -25,6 +26,24 @@ def copy_files(src, dst):
     for file in src:
         shutil.copy(file, dst)
 
+def categorize_data(dataset, label_table):   
+    for file in dataset:
+        #files.append(os.path.splitext(f)[0])
+        path = os.path.dirname(file)
+        filename = os.path.basename(file).split('.')[0]
+        cow_id = filename.split('-')[0]
+        tag = (filename.split('-')[1]).split('s')[0]
+        label = classify(label_table[(label_table['cow_id'] == cow_id)].iloc[0, tag_loc(int(tag))])
+
+        if label == 1:
+            shutil.move(file, path + '/LS1')
+        elif label == 2:
+            shutil.move(file, path + '/LS2')
+        elif label == 3:
+            shutil.move(file, path + '/LS3')
+        elif label == 4:
+            shutil.move(file, path + '/LS4')
+
 """ 
 Functions for statistical analysis 
   * cm: confusion matrix created from sklearn.metrics
@@ -38,10 +57,13 @@ def precision(cm):
 def recall(cm):
     return (cm.diagonal() / np.sum(cm, axis=1))
 
-def f_score(cm)
+def f_score(cm):
+    p = precision(cm)
+    r = recall(cm)
+    return 2 * (p * r) / (p + r)
 
-def print_analysis(y_true, y_pred):
-    cm = confusion_matrix(y_true, y_pred, labels=[0, 1, 2, 3])
+def print_analysis(cm):
+    #cm = confusion_matrix(y_true, y_pred, labels=[0, 1, 2, 3])
     print('Confusion matrix:')
     print(cm, type(cm))
     print("Accuracy: ", accuracy(cm))
@@ -49,4 +71,4 @@ def print_analysis(y_true, y_pred):
     print("label precision recall f-score")
     for label in range(4):
         print("{0:5d} {1:7.3f} {2:8.3f} {3:6.3f}".format(label, precision(cm)[label], recall(cm)[label], 
-                                                     f1_score(y_true, y_pred, average=None)[label]))
+                                                     f_score(cm)[label]))
